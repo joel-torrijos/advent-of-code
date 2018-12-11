6ab1af71c21aca3872b1cd6ec1c34b5070550ef2
Jose Emmanuel Torrijos <jose.emmanuel.torrijos@gmail.com>
2018-12-12 01:29:45 +0800

    Summed-area table solution for Day 11 Part 2

--- src/main/java/com/joeltorrijos/year2018/day1/Day11.java
+++ src/main/java/com/joeltorrijos/year2018/day1/Day11.java
@@ -9,7 +9,7 @@ public class Day11 {
 	public static void main(String[] args) {
 
 		System.out.println(partOne(3613));
-		System.out.println(partTwo(3613));
+		System.out.println(partTwoSumTable(3613));
 		
 	}
 	
@@ -46,8 +46,6 @@ public class Day11 {
 	}
 	
 	public static String partTwo(int serialNumber) {
-		
-		// TODO summed-area table
 		
 		int gridSize = 300;
 		Point[][] grid = new Point[gridSize][gridSize];
@@ -104,6 +102,79 @@ public class Day11 {
 		return maxPowerLevelEntry.getKey();
 	}
 
+	// Summed-area table solution for Part Two
+	public static String partTwoSumTable(int serialNumber) {
+		
+		int gridSize = 300;
+
+		Point[][] grid = new Point[gridSize][gridSize];
+		
+		for(int y1 = 0 ; y1 < grid.length; y1++) {
+			for(int x1 = 0; x1 <  grid[y1].length; x1++) {
+				grid[y1][x1]  = new Point(x1,y1, serialNumber);
+			}
+		}
+		
+		Map<String, Integer> areaPowerLevels = new HashMap<>();
+		
+		int[][] summedAreaTable = new int[gridSize][gridSize];
+		
+		// setup
+		for(int x = 0; x < summedAreaTable.length; x++) {
+			summedAreaTable[0][x] = grid[0][x].powerLevel;
+		}
+		
+		// Sum top to bottom
+		for(int y = 1; y < summedAreaTable.length; y++) {
+			for(int x = 0; x <  summedAreaTable[y].length; x++) {
+				summedAreaTable[y][x] = grid[y][x].powerLevel + summedAreaTable[y-1][x];
+			}
+		}
+		
+		// Sum left to right
+		for(int y = 0; y < summedAreaTable.length; y++) {
+			for(int x = 1; x <  summedAreaTable[y].length; x++) {
+				summedAreaTable[y][x] += summedAreaTable[y][x-1];
+			}
+		}
+		
+		for(int y = 0 ; y < grid.length; y++) {
+			for(int x = 0 ; x < grid[y].length; x++) {
+				int[] minimums = {grid.length - y, grid[y].length - x};
+				int min = Arrays.stream(minimums).min().getAsInt();
+
+				for(int size = 1; size <= min; size++) {
+					int areaPowerLevel = 0;
+					String identifier = x + "," + y  + "," + size;
+					
+					if(size == 1) {
+						areaPowerLevel = grid[y][x].powerLevel;
+					} else {
+						int maxX = x + size - 1;
+						int maxY = y + size - 1;
+						
+						int leftX = x - 1;
+						int topY = maxY - size;
+						
+						int topLeft = (topY < 0 || leftX < 0) ? 0 : summedAreaTable[topY][leftX];
+						int bottomLeft = (maxY < 0 || leftX < 0) ? 0 : summedAreaTable[maxY][leftX];
+						int topRight = (maxY < 0 || topY < 0) ? 0 : summedAreaTable[topY][maxX];
+						int bottomRight = (maxY < 0 || maxX < 0) ? 0 : summedAreaTable[maxY][maxX];
+						
+						areaPowerLevel = bottomRight - topRight - bottomLeft + topLeft;
+					}
+					
+					areaPowerLevels.put(identifier, areaPowerLevel);						
+				}
+			}
+		}
+		
+		
+		Map.Entry<String, Integer> maxPowerLevelEntry = areaPowerLevels.entrySet().stream().max(Map.Entry.comparingByValue()).get();
+
+		return maxPowerLevelEntry.getKey();
+	}
+	
 	static class Point {
 		int x, y, serialNum, powerLevel;
 
--- src/test/java/com/joeltorrijos/year2018/day1/Day11Test.java
+++ src/test/java/com/joeltorrijos/year2018/day1/Day11Test.java
@@ -2,6 +2,7 @@ package com.joeltorrijos.year2018.day1;
 
 import static com.joeltorrijos.year2018.day1.Day11.partOne;
 import static com.joeltorrijos.year2018.day1.Day11.partTwo;
+import static com.joeltorrijos.year2018.day1.Day11.partTwoSumTable;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,12 @@ public class Day11Test {
 	void testTwo() {
 		assertEquals("90,269,16", partTwo(18));
 		assertEquals("232,251,12", partTwo(42));
+	}
+	
+	@Test
+	void testTwoSumTable() {
+		assertEquals("90,269,16", partTwoSumTable(18));
+		assertEquals("232,251,12", partTwoSumTable(42));
+		
 	}
 }
